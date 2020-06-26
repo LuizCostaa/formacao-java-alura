96c64d8720428a4614ac115d35b4e03567bebb3c
Luiz Guilherme <luizguilherme.lgsc@gmail.com>
2020-06-25 22:08:30 -0300

    Update FluxoComTratamento.java

--- java-pilha/src/FluxoComTratamento.java
+++ java-pilha/src/FluxoComTratamento.java
@@ -5,7 +5,7 @@ public class FluxoComTratamento {
 		System.out.println("ini main");
 		try { 
 			metodo1();			
-		} catch (ArithmeticException | NullPointerException ex) {
+		} catch (Exception ex) {
 			String msg = ex.getMessage();
 			System.out.println("Exception" + msg);
 			ex.printStackTrace();
@@ -13,14 +13,14 @@ public class FluxoComTratamento {
 		System.out.println("Fim do main");
 	}
 	
-	private static void metodo1() {
+	private static void metodo1() throws MinhaExcecao {
 		System.out.println("ini metodo 1");		
 		metodo2();
 		
 		System.out.println("Fim metodo 1");
 	}
 	
-	private static void metodo2() {
+	private static void metodo2() throws MinhaExcecao {
 		System.out.println("ini metodo 2");
 		for (int i = 1; i<= 5; i++) {	 
 			System.out.println(i);
