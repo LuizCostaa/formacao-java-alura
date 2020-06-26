25ce383812fe497c4036f4c0300496f1b770324a
Luiz Guilherme <luizguilherme.lgsc@gmail.com>
2020-06-25 21:53:33 -0300

    Capturando qualquer exceção

--- java-pilha/src/Fluxo.java
+++ java-pilha/src/Fluxo.java
@@ -5,7 +5,7 @@ public class Fluxo {
 		System.out.println("ini main");
 		try { 
 			metodo1();			
-		} catch (ArithmeticException | NullPointerException | MinhaExcecao ex) {
+		} catch (Exception ex) {
 			String msg = ex.getMessage();
 			System.out.println("Exception " + msg);
 			ex.printStackTrace();
